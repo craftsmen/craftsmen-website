6f1336f4c94066d6b7c0aa51a5c14c1894ce5336
Mehdi Lahmam <mehdi@craftsmen.io>
2016-04-19 18:06:19 +0200

    :wave: :metal: :kissing_heart:

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "bourbon"
+gem "middleman", "~> 4.0"
+gem "middleman-autoprefixer"
+gem "middleman-deploy", "= 2.0.0.pre.alpha"
+gem "middleman-livereload"
+gem "middleman-sprockets", "= 4.0.0.rc.1"
+gem "neat"
---
+++ config.rb
@@ -0,0 +1,23 @@
+activate :directory_indexes
+activate :autoprefixer
+
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :layout, "layouts/application"
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  # Relative assets needed to deploy to GitHub Pages
+  activate :relative_assets
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
